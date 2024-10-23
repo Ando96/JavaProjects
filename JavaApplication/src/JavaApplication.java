8ef0822d006e8e69a844310907753f5b9f227b17
Ryan Anderson <49941690+Ando96@users.noreply.github.com>
2024-10-23 20:18:55 +0100

    Add files via upload

---
+++ JavaApplication/src/JavaApplication.java
@@ -0,0 +1,21 @@
+
+ 
+ 
+ 
+
+
+
+/**
+ *
+ * @author EC1401794
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
